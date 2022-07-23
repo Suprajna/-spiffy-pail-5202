853374ef6dd51286431eb76df3afb5fb6ecea341
Suprajna <suprajnaks@gmail.com>
2022-07-23 20:24:36 +0530

    saved

--- prductpage.js
+++ prductpage.js
@@ -206,11 +206,353 @@ let productData=[
     catagory: "Mangoes",
     img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/158/original/data?width=256&height=256",
   },
+    {
+    name: "Disco Pumpkin",
+    catagory: "Daily Vegies",
+    price: "₹25",
+    quantity:"250 g",
+    productID: "veg1",
+   
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/599/original/data?width=256&height=256",
+  },
+  {
+   
+   
+    name: "Ladies Finger/Bhindi",
+    catagory: "Daily Vegies",
+    price: "₹47",
+    quantity:"500 g",
+    productID: "veg2",
+ 
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/006/624/original/data?width=256&height=256",
+  },
+  {
+   
+    name: "Beetroot",
+    price: "₹21",
+    quantity:"500 g",
+    productID: "veg3",
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VBEE12.png?width=256&height=256",
+  },
+  {
+   
+    name: "Sweet Potato",
+    price: "₹59",
+    quantity:"500 g",
+    productID: "veg4",
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VSWE12.png?width=256&height=256",
+  },
+  {
+   
+    name: " Arbi/Colocasia",
+    price: "₹40",
+    quantity:"500 g",
+    productID: "veg5",
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VARB12.png?width=256&height=256",
+  },
+  {
+    
+    name: "Tomato",
+    price: "₹36",
+    quantity:"1 kg",
+    productID: "veg6",
+    catagory: "Daily Vegies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/006/605/original/data?width=256&height=256",
+  },
+  
+  {
+   
+    name: "Yam(Elephant Foot)",
+    price: "₹52",
+    quantity:"500 g",
+    productID: "veg7",
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VSUR12.png?width=256&height=256",
+  },
+  {
+    
+    name: "Banana Stem",
+    price: "₹50",
+    quantity:"1 pc",
+    productID: "veg8",
+    catagory: "Daily Vegies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/009/241/original/data?width=256&height=256",
+  },
+  {
+  
+    name: "Orange Carrot",
+    price: "₹46",
+    quantity:"500 g",
+    productID: "veg9",
+  
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/FCAR1.png?width=256&height=256"
+  },
+  {
+    name: "Onion",
+    price: "₹47",
+    quantity:"1 kg",
+    productID: "veg10",
+   
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/BONI11.png?width=256&height=256",
+  },
+  {
+    name: "Potato",
+    price: "₹40",
+    productID: "veg11",
+    quantity:"1 kg",
+    catagory: "Daily Vegies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/BPOT11.png?width=256&height=256",
+  },
+  {
+    name: "Pumpkin",
+    price: "₹30",
+    productID: "veg12",
+    quantity:"500 g",
+    catagory: "Daily Vegies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/101/original/data?width=256&height=256",
+  },
+  {
+    name: "Betel Leaf",
+    price: "₹55",
+    productID: "veg13",
+    quantity:"12 pc",
+    catagory: "Herbs and Leafies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/602/original/data?width=256&height=256",
+  },
+  {
+    name: "Colocasia Leaves",
+    price: "₹6",
+    productID: "veg14",
+    quantity:"1 pc",
+    catagory: "Herbs and Leafies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/419/original/data?width=256&height=256",
+  },
+  {
+    name: "Lemon",
+    price: "₹15",
+    productID: "veg15",
+    quantity:"100 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/003/250/original/data?width=256&height=256",
+  },
+  {
+    name: "Ginger",
+    price: "₹10",
+    productID: "veg16",
+    quantity:"100 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VGIN12.png?width=256&height=256",
+  },
+  {
+    name: "Curry Leaves",
+    price: "₹9",
+    productID: "veg17",
+    quantity:"50 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/LCUR11.png?width=256&height=256",
+  },
+  {
+    name: "Green Chillies",
+    price: "₹15",
+    productID: "veg18",
+    quantity:"100 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VCHI22.png?width=256&height=256",
+  },
+  {
+    name: "Grlic Bunch",
+    price: "₹12",
+    productID: "veg19",
+    quantity:"100 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VGAR12.png?width=256&height=256",
+  },
+  {
+    name: "Coriander Leaves",
+    price: "₹22",
+    productID: "veg20",
+    quantity:"100 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/LCOR21.png?width=256&height=256",
+  },
+  {
+    name: "Methi(Fenugreek Leaves)",
+    price: "₹19",
+    productID: "veg21",
+    quantity:"100 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/LMET1.png?width=256&height=256",
+  },
+  {
+    name: "Turmeric",
+    price: "₹36",
+    productID: "veg22",
+    quantity:"250 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/375/original/data?width=256&height=256"
+  },
+  {
+    name: "Alovera",
+    price: "₹42",
+    productID: "veg23",
+    quantity:"1 pc",
+    catagory: "Herbs and Leafies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/417/original/data?width=256&height=256",
+  },
+  {
+    name: "Red Amaranthus",
+    price: "₹42",
+    productID: "veg24",
+    quantity:"250 g",
+    catagory: "Herbs and Leafies",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/162/original/data?width=256&height=256",
+  },
+  {
+    
+    name: "Portobello Mushroom",
+    catagory: "Exotic Vegetable",
+    price: "₹69",
+    quantity:"1 pc",
+    productID: "veg25",
+   
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/008/454/original/data?width=256&height=256",
+  },
+  {
+   
+   
+    name: "Button Mushroom",
+    catagory: "Exotic Vegetable",
+    price: "₹45",
+    quantity:"1 pkt",
+    productID: "veg26",
+ 
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/EMUS2.png?width=256&height=256",
+  },
+  {
+   
+    name: "Yellow Capsicum(Bell Pepper)",
+    price: "₹125",
+    quantity:"2 pc",
+    productID: "veg27",
+    catagory: "Exotic Vegetable",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/387/original/data?width=256&height=256",
+  },
+  {
+   
+    name: "Red Capsicum",
+    price: "₹125",
+    quantity:"2 pc",
+    productID: "veg28",
+    catagory: "Exotic Vegetable",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/386/original/data?width=256&height=256",
+  },
+  {
+   
+    name: " Green Zucchini",
+    price: "₹62",
+    quantity:"1 pc",
+    productID: "veg29",
+    catagory: "Exotic Vegetable",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/EZUC12.png?width=256&height=256",
+  },
+  {
+    
+    name: "Yellow Zucchini",
+    price: "₹75",
+    quantity:"1 pc",
+    productID: "veg30",
+    catagory: "Exotic Vegetable",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/EZUC32.png?width=256&height=256",
+  },
+  
+  {
+   
+    name: "Lettuce Iceberg",
+    price: "₹45",
+    quantity:"1 pc",
+    productID: "veg31",
+    catagory: "Exotic Vegetable",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/ELET22.png?width=256&height=256",
+  },
+  {
+    
+    name: "Red Cabbage",
+    price: "₹59",
+    quantity:"1 pc",
+    productID: "veg32",
+    catagory: "Exotic Vegetable",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VCAB22.png?width=256&height=256",
+  },
+  {
+  
+    name: "Pulao Combo",
+    price: "₹80",
+    quantity:"200 g",
+    productID: "veg33",
+  
+    catagory: "Cuts,Peeled and Sprout",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/320/original/data?width=256&height=256"
+  },
+  {
+    name: "Peeled Garlic",
+    price: "₹35",
+    quantity:"100 g",
+    productID: "veg34",
+   
+    catagory: "Cuts,Peeled and Sprout",
+    img: "https://imagemaster.fraazo.com/fraazo-master/products/VGAR32.png?width=256&height=256",
+  },
+  {
+    name: "UPVAS Combo(500g sweet potato+500 g arbi+1kg potato)",
+    price: "₹139",
+    productID: "veg35",
+    quantity:"1 combo",
+    catagory: "Vegetable Combos",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/202/original/data?width=256&height=256",
+  },
+  {
+    name: "Fresh Salad Combo(500 g Carrot+500 g Cucumber)",
+    price: "₹78",
+    productID: "veg36",
+    quantity:"1 combo",
+    catagory: "Vegetable Combos",
+    img: "https://imageprod.fraazo.com/fraazo-prod/products/product_images/000/000/389/original/data?width=256&height=256",
+  },
+  {
+    name: "Regular Vegie Combo(1 pc Cabbage+1 p Cauliflower)",
+    price: "₹152",
+    productID: "veg37",
+    quantity:"1kgs combo",
+    catagory: "Vegetable Combos",
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/008/980/original/data?width=256&height=256",
+  },
+  {
+    name: "Masala Combo",
+    price: "₹49",
+    productID: "veg38",
+    quantity:"350 g",
+    catagory: "Vegetable Combos",
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/009/177/original/data?width=256&height=256",
+  },
+  {
+    name: "Detox Mix Combo(500 g Carrot+500 g Beetroot)",
+    price: "₹67",
+    productID: "veg39",
+    quantity:"1 combo",
+    catagory: "Vegetable Combos",
+    img: "https://imageprod.fraazo.com/fraazo-prod/images/images/000/009/175/original/data?width=256&height=256",
+  },
   
 ]
 let productsCont=document.getElementById("products");
 let filter=document.getElementById("filter");
-let cartLs=JSON.parse(localStorage.getItem("cart-page"))||[];
+
+let cartLS=JSON.parse(localStorage.getItem("cart"))||[];
 function displayProducts(productData){
    productsCont.innerHTML="";
     productData.forEach(function(elem){
@@ -236,8 +578,8 @@ function displayProducts(productData){
         addToCart.addEventListener("click",function(){
            if(addToCartFunc(elem.productID)===true){
                alert("Product Added Successfully");
-               cartLs.push(elem);
-               localStorage.setItem("cart-page",JSON.stringify(cartLs));
+               cartLS.push(elem);
+               localStorage.setItem("cart",JSON.stringify(cartLS));
            }else{
                alert("Product Already Exist");
            }
@@ -258,9 +600,22 @@ filter.addEventListener("change",function(){
         displayProducts(filtered);
     }
 })
+displayProducts(productData);
+filter.addEventListener("change",function(){
+    if(filter.value==="All"){
+        displayProducts(productData);  
+    }else{
+        let filtered1=productData.filter1(function(elem){
+            return elem.catagory===filter.value;
+        })
+        displayProducts(filtered1);
+    }
+})
+
+
 function addToCartFunc(id){
-for(let i=0;i<cartLs.length;i++){
-        if(cartLs[i].productId==id){
+for(let i=0;i<cartLS.length;i++){
+        if(cartLS[i].productId==id){
             return false;
         }
 }
